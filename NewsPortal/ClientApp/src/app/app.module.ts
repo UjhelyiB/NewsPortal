bb2209613d1cabb5cfe9b2676b6eb66b2b80a554
Ujhelyi Benedek <ujhelyi.benedek@gmail.com>
2018-11-25 16:44:33 +0100

    Facebook integration, proper navigation to detailed pages, translating things to Hungarian, restricting access to editor pages, hiding them from plain sight.

--- NewsPortal/ClientApp/src/app/app.module.ts
+++ NewsPortal/ClientApp/src/app/app.module.ts
@@ -40,10 +40,11 @@ import { Login } from './login/login.component';
 import { CategoryService } from '../services/category.service';
 import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
 import { EditDialogComponent } from './edit-dialog/edit-dialog.component';
-import { EditNewsComponent } from './edit-news/edit-news.component';
 import { AuthGuard } from '../services/auth-guard.service';
 import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+
+import { JwSocialButtonsModule } from 'jw-angular-social-buttons';
 
 export function tokenGetter() {
     return localStorage.getItem('jwt');
@@ -64,8 +65,7 @@ export function tokenGetter() {
         CategoryComponent,
         Login,
         DeleteDialogComponent,
-        EditDialogComponent,
-        EditNewsComponent
+        EditDialogComponent
     ],
     imports: [
         BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -83,9 +83,11 @@ export function tokenGetter() {
         MatDialogModule,
         MatSelectModule,
         MatSnackBarModule,
+        MatTooltipModule,
         MatSliderModule,
         MatSortModule,
         CKEditorModule,
+        JwSocialButtonsModule,
         RouterModule.forRoot([
             { path: '', component: HomeComponent, pathMatch: 'full' },
             { path: 'counter', component: CounterComponent },
@@ -93,10 +95,9 @@ export function tokenGetter() {
             { path: 'post/:id', component: NewsPostComponent },
             { path: 'search', component: SearchComponent },
             { path: 'category', component: CategoryComponent },
-            { path: 'category-editor', component: CategoryEditorComponent },
-            { path: 'news-editor', component: NewsEditorComponent },
+            { path: 'category-editor', component: CategoryEditorComponent, canActivate: [AuthGuard] },
+            { path: 'news-editor', component: NewsEditorComponent, canActivate: [AuthGuard] },
             { path: 'login', component: Login },
-            { path: 'edit-news', component: EditNewsComponent, canActivate: [AuthGuard] },
         ]),
         JwtModule.forRoot({
             config: {
